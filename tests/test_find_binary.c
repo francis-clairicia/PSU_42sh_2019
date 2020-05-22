f2b5cc02d75d403cf62aa73a731f41f3eb631b49
Francis Clairicia-Rose-Claire-Josephine <fclairicia-rcj@frankysnow.fr>
2020-05-22 13:13:14 +0200

    [FIX] New test for path handling done

--- tests/test_find_binary.c
+++ tests/test_find_binary.c
@@ -11,7 +11,8 @@
 
 Test(find_binary_in_path, find_out_a_binary_in_path_environment)
 {
-    char *path = find_binary_in_path("ls", DEFAULT_ENVIRONMENT);
+    char *path_list[] = {"/usr/bin", "/bin", NULL};
+    char *path = find_binary_in_path("ls", path_list);
 
     cr_assert_not_null(path);
     cr_expect(my_strcmp(path, "/usr/bin/ls") == 0
@@ -21,20 +22,22 @@ Test(find_binary_in_path, find_out_a_binary_in_path_environment)
 
 Test(find_binary_in_path, print_error_when_not_found)
 {
+    char *path_list[] = {"/usr/bin", "/bin", NULL};
     char *path = NULL;
 
     cr_redirect_stderr();
-    path = find_binary_in_path("unknown_bin", DEFAULT_ENVIRONMENT);
+    path = find_binary_in_path("unknown_bin", path_list);
     cr_expect_null(path);
     cr_expect_stderr_eq_str("unknown_bin: Command not found.\n");
 }
 
 Test(find_binary_in_path, handle_null_binary)
 {
+    char *path_list[] = {"/usr/bin", "/bin", NULL};
     char *path = NULL;
 
     cr_redirect_stderr();
-    path = find_binary_in_path(NULL, DEFAULT_ENVIRONMENT);
+    path = find_binary_in_path(NULL, path_list);
     cr_expect_null(path);
     cr_expect_stderr_eq_str("NULL: Command not found.\n");
 }
@@ -42,10 +45,10 @@ Test(find_binary_in_path, handle_null_binary)
 Test(find_binary_in_path, print_error_if_path_is_not_set)
 {
     char *path = NULL;
-    char *envp[] = {NULL};
+    char *path_list[] = {NULL};
 
     cr_redirect_stderr();
-    path = find_binary_in_path("ls", envp);
+    path = find_binary_in_path("ls", path_list);
     cr_expect_null(path);
     cr_expect_stderr_eq_str("ls: Command not found.\n");
 }
