1fae8336c76b00e53d4734d0fad49119595b14bf
Jimmy <jimmy.kohl@epitech.eu>
2020-05-24 15:40:26 +0200

    [FIX] valgrind error destroy every alias [ADD] Call function destroy alias when destroy shell

--- src/shell_struct/destroy_shell.c
+++ src/shell_struct/destroy_shell.c
@@ -13,6 +13,7 @@ void destroy_shell_struct(shell_t *shell)
         return;
     my_free_array(shell->envp);
     free_var_list(shell->var_list);
+    destroy_every_alias(&shell->alias_list);
     my_free_list(&(shell->process), &destroy_process_struct);
     my_free_list(&(shell->history), &free);
     free(shell);
