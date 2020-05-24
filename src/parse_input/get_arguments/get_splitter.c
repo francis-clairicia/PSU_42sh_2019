12f236af54ab008b0eddc9cf2f6c30772d02e8de
gbogard <guillaume.bogard-coquard@epitech.eu>
2020-05-24 16:59:03 +0200

    [Fix] -> Splitters don't create useless nodes if the input ends after their position in input

--- src/parse_input/get_arguments/get_splitter.c
+++ src/parse_input/get_arguments/get_splitter.c
@@ -25,8 +25,10 @@ static bool set_splitter_alterations(parse_list_t **head,
     if (!((*head)->cmd_list->args) || is_char_splitter(c))
         return (true);
     (*head)->prev->splitter = splitter_type;
-    add_parsed_list_node(head);
-    add_cmd_list_node(&((*head)->prev->cmd_list));
+    if (c) {
+        add_parsed_list_node(head);
+        add_cmd_list_node(&((*head)->prev->cmd_list));
+    }
     return (true);
 }
 
